50ca5a8e397765f55dc19ea2849d9c64960de076
Per <perlewerenz@gmx.de>
2025-03-16 13:59:38 +0100

    make user birthday null=True

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ from .managers import CustomUserManager
 class CustomUser(AbstractUser):
     #username = None
     email = models.EmailField(_("email address"), unique=True)
-    birthdate = models.DateField()
+    birthdate = models.DateField(null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
