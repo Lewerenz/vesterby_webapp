20a309fb30a0879405d1600068943073259b9f8a
Per <perlewerenz@gmx.de>
2025-03-21 22:11:57 +0100

    add event and visit models

--- schedule/urls.py
+++ schedule/urls.py
@@ -3,5 +3,17 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('<slug:pk>/', views.EventDetailView.as_view()),
+    
+    path('event/create', views.EventCreateView.as_view(), name='event_create'),
+    path('event/read/<int:pk>', views.EventReadView.as_view(), name='event_read'),
+    path('event/update/<int:pk>', views.EventUpdateView.as_view(), name='event_update'),
+    path('event/delete/<int:pk>', views.EventDeleteView.as_view(), name='event_delete'),
+    path('event/', views.EventList.as_view(), name='event_list'), 
+
+    path('visit/create', views.VisitCreateView.as_view(), name='visit_create'),
+    path('visit/read/<int:pk>', views.VisitReadView.as_view(), name='visit_read'),
+    path('visit/update/<int:pk>', views.VisitUpdateView.as_view(), name='visit_update'),
+    path('visit/delete/<int:pk>', views.VisitDeleteView.as_view(), name='visit_delete'),
+    path('visit/', views.VisitList.as_view(), name='visit_list'), 
+
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,11 +1,77 @@
-from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from bootstrap_datepicker_plus.widgets import DateTimePickerInput, DatePickerInput
 
 # Create your views here.
 
-from django.views.generic.detail import DetailView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from .models import event
+from .models import Event, Visit
 
 
-class EventDetailView(DetailView):
-    model = event
+## Event
+# Create
+class EventCreateView(LoginRequiredMixin, CreateView):
+    model = Event
+    fields = '__all__'
+    success_url = reverse_lazy('event_list')
+
+# Read
+class EventReadView(LoginRequiredMixin, DetailView):
+    model = Event
+
+# Update
+class EventUpdateView(LoginRequiredMixin, UpdateView):
+    model = Event
+    fields = '__all__'
+    success_url = reverse_lazy('event_list')
+
+# Delete
+class EventDeleteView(LoginRequiredMixin, DeleteView):
+    model = Event
+    success_url = reverse_lazy('event_list')
+
+# List
+class EventList(LoginRequiredMixin, ListView):
+    model = Event
+
+
+## Visit
+# Create
+class VisitCreateView(LoginRequiredMixin, CreateView):
+    model = Visit
+    fields = '__all__'
+    success_url = reverse_lazy('visit_list')
+    def get_form(self):
+        form = super().get_form()
+        form.fields['startdate'].widget = DatePickerInput()
+        form.fields['enddate'].widget = DatePickerInput()
+        return form
+
+
+# Read
+class VisitReadView(LoginRequiredMixin, DetailView):
+    model = Visit
+
+# Update
+class VisitUpdateView(LoginRequiredMixin, UpdateView):
+    model = Visit
+    fields = '__all__'
+    success_url = reverse_lazy('visit_list')
+    def get_form(self):
+        form = super().get_form()
+        form.fields['startdate'].widget = DatePickerInput()
+        form.fields['enddate'].widget = DatePickerInput()
+        return form
+
+# Delete
+class VisitDeleteView(LoginRequiredMixin, DeleteView):
+    model = Visit
+    success_url = reverse_lazy('visit_list')
+
+# List
+class VisitList(LoginRequiredMixin, ListView):
+    model = Visit
